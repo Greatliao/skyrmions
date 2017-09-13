42081a7142fc5fc08fd2e192d53112417dec6abf
greatliao <260351042@qq.com>
2017-09-13 17:09:20 +0800

    update

--- skyrmion/TPEM/diag.h
+++
@@ -1,4 +0,0 @@
-
-typedef struct {double re,im; }  ccomplex;
-void diag(int n, ccomplex *a, double *e);
-
--- skyrmion/nonfermi/data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-num=10
-filename=nonfermi
-for (( i=1; i<= $num; i++ ))
-do
-    cp $filename$i/lattice.out data/lattice_$i.out
-done
