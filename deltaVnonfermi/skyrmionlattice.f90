9501140234f59dbf862a846bf127fb0a74f4abdc
greatliao <260351042@qq.com>
2017-09-25 20:37:13 +0800

    add the energy and T the plot to see

--- deltaVnonfermi/skyrmionlattice.f90
+++ deltaVnonfermi/skyrmionlattice.f90
@@ -169,12 +169,12 @@ program skyrmionlattice
     !N = L*L, num of lattice
     !t, J_k, the parameter if H
     !##########################################
-    integer, parameter :: L = 40, N = L*L, num_MC = 4*10**5, num_T_para = 20
+    integer, parameter :: L = 40, N = L*L, num_MC = 4*10**5, num_T_para = 30
     real*8, parameter :: t = 1.0, PI = 3.141592654
     integer :: i, j, k, ii, r, d, di, dd, ij, aver_num
     integer :: p, p_i_pl, p_i_mi, p_j_pl, p_j_mi, p_i_pl_j_mi, p_i_mi_j_pl
     real*8 :: ran, beta, Tc, x, Hp, B_z
-    real*8 :: V, deltaV, J_H, lambda, pa, V_new
+    real*8 :: V, deltaV, J_H, lambda, pa, energy_aver, energy_aver_B
     integer, dimension(1) :: mloc
     real*8, dimension(2,N) :: fai, theta
     real*8, dimension(num_T_para) :: T_para
@@ -188,13 +188,9 @@ program skyrmionlattice
     read(12,*) pa
     close(12)
     B_z = pa*t
-    T_para = (/ 1.0, 0.95, 0.9, 0.85, 0.8, 0.75, 0.7, 0.65, 0.6, 0.55, 0.5, 0.45, 0.4, 0.35, 0.3, 0.25, 0.2, 0.15, 0.1, 0.05/)
-    open(unit = 10, file = 'T.out')
-    do i = 1, num_T_para
-        write(10,*) T_para(i)
-    end do
-    close(10)
-
+    T_para = (/ 1.0, 0.95, 0.9, 0.85, 0.8, 0.75, 0.7, 0.65, 0.6, 0.55, 0.5, 0.45, 0.4, 0.35, 0.3, 0.25, 0.2, 0.15, 0.1, 0.08,&
+        0.06, 0.04, 0.02, 0.008, 0.006, 0.004, 0.002, 0.0008, 0.0006, 0.0004/)
+    open(unit = 10, file = 'T_energyaver.out')
     call initran(1)
 
     do i = 1, N
@@ -203,24 +199,22 @@ program skyrmionlattice
         x = ran()
         theta(d,i) = x*PI
     end do
-    !ij = 0
+    call calcu_V( L, J_H, lambda, fai(d,:), theta(d,:), H, V )
 do ii = 1, num_T_para
+    energy_aver = 0.0
+    energy_aver_B = 0.0
     write(cha,'(i2)') ii
     open(unit = 13, file = 'energy'//adjustl(trim(cha))//'.out' )
     open( unit = 14, file = 'lattice'//adjustl(trim(cha))//'.out' )
     write(*,*) num_T_para-ii
     Tc = T_para(ii)*J_H
     beta = 1/Tc
-    !r= 0
-    call calcu_V( L, J_H, lambda, fai(d,:), theta(d,:), H, V )
 
     do i = 1, num_MC 
 
         dd = di(d)
         x = ran()
         r = int(x*N)+1 
-        !r = r+1
-        !Write(*,*) r
         fai(dd,:) = fai(d,:)
         theta(dd,:) = theta(d,:)
         x = ran()
@@ -261,21 +255,21 @@ do ii = 1, num_T_para
             call calcu_Hp( p_i_mi_j_pl, L, J_H, lambda, fai(dd,:), theta(dd,:), H(p_i_mi_j_pl))
         end if
         d = dd
-        !if (ii == num_T_para .AND. i > num_MC-aver_num*N ) then
         if ( i > num_MC-aver_num*N ) then
             if ( mod(i+aver_num*N-num_MC, N) == 0 ) then
+                energy_aver = energy_aver+V
+                call calcu_deltaV(L, N, B_z, theta(d,:), deltaV)
+                energy_aver_B = energy_aver_B+V+deltav
                 do j = 1, N
                     write(14,*) theta(d,j), fai(d,j)
                 end do
             end if
         end if
-        !if ( r == N ) then
-        !    r = 0
-        !end if
         write(13,*) V
     end do
     close(13)
     close(14)
+    write(10,*) T_para(ii), energy_aver/aver_num, energy_aver_B/aver_num
 end do
 
     write(*,*) 'end'
